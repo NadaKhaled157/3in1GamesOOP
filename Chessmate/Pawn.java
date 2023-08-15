6dbaaf605cef29b368af53ee652deb77677beac9
NadaKhaled157 <nadakhaled9277@gmail.com>
2023-08-15 23:51:40 +0300

    Pawn Update

--- Chessmate/Pawn.java
+++ Chessmate/Pawn.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 
 public class Pawn extends Piece {
 
+    boolean isFirstMove; //used in en passant
+    ArrayList<Piece> threatenedPawn=new ArrayList<>(); //threatened by enPassant
+    ArrayList<Tile> enPassantTile= new ArrayList<>(); //tile available as possible move when en passant is allowed
+
     Pawn(int x, int y, PieceColor pieceColor) {
         this.x = x;
         this.y = y;
@@ -12,25 +16,26 @@ public class Pawn extends Piece {
     }
 
     public ArrayList<Tile> findPossibleMoves(Board currentBoard) {
-        int[]X={this.x+1,this.x-1};
+        int[] X = {this.x + 1, this.x - 1};
         // White Pawn
         if (this.pieceColor.equals(PieceColor.White)) {
-            int[] Y={this.y+1,this.y+1};
+            int[] Y = {this.y + 1, this.y + 1};
             //First Move
             if (this.y == 1) {
-                if (!currentBoard.boardTiles[this.x][this.y+2].hasPiece) {
-                    this.possibleMoves.add(currentBoard.boardTiles[this.x][this.y + 2]);
+                if (!currentBoard.boardTiles[this.x][this.y + 2].hasPiece) {
+                    if (!currentBoard.boardTiles[this.x][this.y + 1].hasPiece)
+                        this.possibleMoves.add(currentBoard.boardTiles[this.x][this.y + 2]);
                 } else
                     this.illegalMoves.add(currentBoard.boardTiles[this.x][this.y + 2]);
             }
             //Regular Move
-            if (y!=7) {
+            if (y != 7) {
                 if (!currentBoard.boardTiles[this.x][this.y + 1].hasPiece)
                     this.possibleMoves.add(currentBoard.boardTiles[this.x][this.y + 1]);
                 else this.illegalMoves.add(currentBoard.boardTiles[this.x][this.y + 1]);
             }
             //Diagonal Attack
-            for (int i=0;i<X.length;i++) {
+            for (int i = 0; i < X.length; i++) {
                 if (X[i] < 0 || X[i] > 7 || Y[i] < 0 || Y[i] > 7)
                     continue;
                 if (currentBoard.boardTiles[X[i]][Y[i]].hasPiece) {
@@ -39,118 +44,95 @@ public class Pawn extends Piece {
                     else this.illegalMoves.add(currentBoard.boardTiles[X[i]][Y[i]]);
                 }
             }
-//            if (x == 0) {
-//                if (currentBoard.boardTiles[this.x+1][this.y+1].hasPiece){
-//                    if (currentBoard.boardTiles[this.x+1][this.y+1].getPiece().pieceColor.equals(PieceColor.Black))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x + 1][this.y + 1]);
-//                    else this.illegalMoves.add(currentBoard.boardTiles[this.x + 1][this.y + 1]);
-//                }
-//            } else if (x==7) {
-//                if (currentBoard.boardTiles[this.x-1][this.y+1].hasPiece){
-//                    if (currentBoard.boardTiles[this.x-1][this.y+1].getPiece().pieceColor.equals(PieceColor.Black))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x - 1][this.y + 1]);
-//                    else this.illegalMoves.add(currentBoard.boardTiles[this.x - 1][this.y + 1]);
-//                }
-//            } else {
-//                if (currentBoard.boardTiles[this.x+1][this.y+1].hasPiece){
-//                    if (currentBoard.boardTiles[this.x+1][this.y+1].getPiece().pieceColor.equals(PieceColor.Black))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x+1][this.y+1]);
-//                    else this.illegalMoves.add(currentBoard.boardTiles[this.x+1][this.y+1]);
-//                }
-//                if (currentBoard.boardTiles[this.x-1][this.y+1].hasPiece){
-//                    if (currentBoard.boardTiles[this.x-1][this.y+1].getPiece().pieceColor.equals(PieceColor.Black))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x-1][this.y+1]);
-//                    else this.illegalMoves.add(currentBoard.boardTiles[this.x-1][this.y+1]);
-//                }
-//            }
+
         }
 
         // Black Pawn
         if (this.pieceColor.equals(PieceColor.Black)) {
-            int[] Y={this.y-1,this.y-1};
-//            if (!currentBoard.boardTiles[x][y+1].hasPiece) {
+            int[] Y = {this.y - 1, this.y - 1};
             //First Move
             if (this.y == 6) {
-                if (!currentBoard.boardTiles[x][y-2].hasPiece) {
-                    this.possibleMoves.add(currentBoard.boardTiles[this.x][this.y-2]);
+                if (!currentBoard.boardTiles[x][y - 2].hasPiece) {
+                    if (!currentBoard.boardTiles[this.x][this.y - 1].hasPiece)
+                        this.possibleMoves.add(currentBoard.boardTiles[this.x][this.y - 2]);
                 } else
-                    this.illegalMoves.add(currentBoard.boardTiles[this.x][this.y-2]);
+                    this.illegalMoves.add(currentBoard.boardTiles[this.x][this.y - 2]);
             }
             //Regular Move
-            if (y!=0) {
+            if (y != 0) {
                 if (!currentBoard.boardTiles[this.x][this.y - 1].hasPiece)
                     this.possibleMoves.add(currentBoard.boardTiles[this.x][this.y - 1]);
                 else this.illegalMoves.add(currentBoard.boardTiles[this.x][this.y - 1]);
             }
             //Diagonal Attack
-            for (int i=0;i<X.length;i++) {
+            for (int i = 0; i < X.length; i++) {
                 if (X[i] < 0 || X[i] > 7 || Y[i] < 0 || Y[i] > 7)
                     continue;
                 if (currentBoard.boardTiles[X[i]][Y[i]].hasPiece) {
                     if (currentBoard.boardTiles[X[i]][Y[i]].getPiece().pieceColor.equals(PieceColor.White))
                         this.possibleMoves.add(currentBoard.boardTiles[X[i]][Y[i]]);
-                    else this.illegalMoves.add(currentBoard.boardTiles[X[i]][Y[i]]);
+                    else this.illegalMoves.add(currentBoard.boardTiles[X[i]][Y[i]]); //maybe remove?
                 }
             }
-//            if (x==0) {
-//                if (currentBoard.boardTiles[this.x+1][this.y-1].hasPiece){
-//                    if (currentBoard.boardTiles[this.x+1][this.y-1].getPiece().pieceColor.equals(PieceColor.White))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x+1][this.y-1]);
-//                    else this.illegalMoves.add(currentBoard.boardTiles[this.x+1][this.y-1]);
-//                }
-//            } else if (x==7) {
-//                if (currentBoard.boardTiles[this.x-1][this.y-1].hasPiece){
-//                    if (currentBoard.boardTiles[this.x-1][this.y-1].getPiece().pieceColor.equals(PieceColor.White))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x-1][this.y-1]);
-//                    else this.illegalMoves.add(currentBoard.boardTiles[this.x-1][this.y-1]);
-//                }
-//            } else {
-//                if (currentBoard.boardTiles[this.x+1][this.y-1].hasPiece){
-//                    if (currentBoard.boardTiles[this.x+1][this.y-1].getPiece().pieceColor.equals(PieceColor.White))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x+1][this.y-1]);
-//                    else this.illegalMoves.add(currentBoard.boardTiles[this.x+1][this.y-1]);
-//                }
-//                if (currentBoard.boardTiles[this.x-1][this.y-1].hasPiece){
-//                    if (currentBoard.boardTiles[this.x-1][this.y-1].getPiece().pieceColor.equals(PieceColor.White))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x-1][this.y-1]);
-//                    else this.illegalMoves.add(currentBoard.boardTiles[this.x-1][this.y-1]);
-//                }
-//            }
         }
-//        if (this.pieceColor.toString().equals("Black")) {
-//            if (!currentBoard.boardTiles[x][y - 1].hasPiece) {
-//                //First Move
-//                if (this.y == 6 && !currentBoard.boardTiles[x][y - 2].hasPiece) {
-//                    this.possibleMoves.add(currentBoard.boardTiles[this.x][this.y - 2]);
-//                }
-//                //Regular Move
-//                this.possibleMoves.add(currentBoard.boardTiles[this.x][this.y - 1]);
-//                //Diagonal Attack
-//                if (x==0){
-//                    if (currentBoard.boardTiles[x + 1][y - 1].hasPiece && currentBoard.boardTiles[x + 1][y - 1].getPiece().pieceColor.equals(PieceColor.White))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x + 1][this.y - 1]);
-//                } else if (x==7){
-//                    if (currentBoard.boardTiles[x - 1][y - 1].hasPiece && currentBoard.boardTiles[x - 1][y - 1].getPiece().pieceColor.equals(PieceColor.White))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x - 1][this.y - 1]);
-//                } else {
-//                    if (currentBoard.boardTiles[x + 1][y - 1].hasPiece && currentBoard.boardTiles[x + 1][y - 1].getPiece().pieceColor.equals(PieceColor.White))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x + 1][this.y - 1]);
-//                    if (currentBoard.boardTiles[x - 1][y - 1].hasPiece && currentBoard.boardTiles[x - 1][y - 1].getPiece().pieceColor.equals(PieceColor.White))
-//                        this.possibleMoves.add(currentBoard.boardTiles[this.x - 1][this.y - 1]);
-//                }
-//            }
-//        }
+        //this.canPerformEnPassant(currentBoard);
         return this.possibleMoves;
     }
 
+    public boolean canPerformEnPassant(Board currentBoard) {
+        //White Pawns
+            if (this.pieceColor.equals(PieceColor.White) && this.y == 4) {
+                if (this.x != 0) {
+                    if (currentBoard.boardTiles[this.x - 1][this.y].getPiece() != null)
+                        if (currentBoard.boardTiles[this.x - 1][this.y].getPiece() instanceof Pawn &&
+                                currentBoard.boardTiles[this.x - 1][this.y].getPiece().pieceColor.equals(PieceColor.Black)) {
+                            threatenedPawn.add(currentBoard.boardTiles[this.x - 1][this.y].getPiece());
+                            enPassantTile.add(currentBoard.boardTiles[this.x - 1][this.y+1]);
+                        return true;
+                        }
+                }
+                if (this.x != 7) {
+                    if (currentBoard.boardTiles[this.x + 1][this.y].getPiece() != null)
+                        if (currentBoard.boardTiles[this.x + 1][this.y].getPiece() instanceof Pawn &&
+                                currentBoard.boardTiles[this.x + 1][this.y].getPiece().pieceColor.equals(PieceColor.Black)) {
+                            threatenedPawn.add(currentBoard.boardTiles[this.x + 1][this.y].getPiece());
+                            enPassantTile.add(currentBoard.boardTiles[this.x + 1][this.y+1]);
+                            return true;
+                        }
+                }
+                System.out.println("ENPASSANT SIZE: "+enPassantTile.size());
+                System.out.println("THREAT SIZE: "+ threatenedPawn.size());
+            }
+        //Black Pawns
+        if (this.pieceColor.equals(PieceColor.Black) && this.y == 3) {
+            if(this.x !=0) {
+                if (currentBoard.boardTiles[this.x - 1][this.y].getPiece() != null)
+                    if (currentBoard.boardTiles[this.x - 1][this.y].getPiece() instanceof Pawn &&
+                            currentBoard.boardTiles[this.x - 1][this.y].getPiece().pieceColor.equals(PieceColor.White)) {
+                        threatenedPawn.add(currentBoard.boardTiles[this.x - 1][this.y].getPiece());
+                        enPassantTile.add(currentBoard.boardTiles[this.x - 1][this.y-1]);
+                        return true;
+                    }
+            }
+            if (this.x !=7) {
+                if (currentBoard.boardTiles[this.x + 1][this.y].getPiece() != null)
+                    if (currentBoard.boardTiles[this.x + 1][this.y].getPiece() instanceof Pawn &&
+                            currentBoard.boardTiles[this.x + 1][this.y].getPiece().pieceColor.equals(PieceColor.White)) {
+                        threatenedPawn.add(currentBoard.boardTiles[this.x + 1][this.y].getPiece());
+                        enPassantTile.add(currentBoard.boardTiles[this.x + 1][this.y-1]);
+                        return true;
+                    }
+            }
+        }
+    return false;}
     public boolean canPromote(Board currentBoard){
         //White Pawn promotion
-        if(currentBoard.boardTiles[x][y].getPiece().pieceColor.equals(PieceColor.White) && this.y==7){
+        if(currentBoard.boardTiles[this.x][this.y].getPiece().pieceColor.equals(PieceColor.White) && this.y==7){
             return true;
         }
 
         //Black Pawn Promotion
-        if (currentBoard.boardTiles[x][y].getPiece().pieceColor.equals(PieceColor.Black) && this.y==0){
+        if (currentBoard.boardTiles[this.x][this.y].getPiece().pieceColor.equals(PieceColor.Black) && this.y==0){
             return true;
         }
         return false;}
